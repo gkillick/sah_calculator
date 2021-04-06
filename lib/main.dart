d04c987835d5a5f66cdd30b96adf9505836516d1
Anthony Killick <anthony@proskida.com>
2021-04-06 10:33:28 -0600

    untested

--- lib/main.dart
+++ lib/main.dart
@@ -2,17 +2,31 @@ import 'package:flutter/material.dart';
 import 'package:sah_calculator/calculator_page.dart';
 import 'package:provider/provider.dart';
 import 'package:sah_calculator/models/risk_factor_brain.dart';
+import 'package:sah_calculator/models/risk_factor.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  const dataDir = "data/";
+  const files = ['1.csv', '2.csv', '3.csv', '4.csv'];
+  List<SetOfTests> testSets = [];
+  files.forEach((fn) async {
+    testSets.add(
+      await getFactorList(dataDir + fn);
+    );
+  });
+  final brain = RiskFactorBrain(testSets)
+  runApp(MyApp(brain));
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  RiskFactorBrain riskFactorBrain;
+  MyApp(brain){
+    this.riskFactorBrain = brain;
+  }
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<RiskFactorBrain>(
-      create: (context) => RiskFactorBrain(),
+      create: (context) => this.riskFactorBrain,
       child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData.light().copyWith(
